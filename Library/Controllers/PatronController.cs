7b344a8021d48621c40da052cce11abaad389cdc
Prince Abraham <prince.vabraham@gmail.com>
2019-09-14 13:51:20 -0400

    Added Patron Interface

---
+++ Library/Controllers/PatronController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Library.Controllers
+{
+    public class PatronController : Controller
+    {
+        private IPatron _patron;
+        public PatronController(IPatron patron)
+        {
+            _patron = patron;
+        }
+        public IActionResult Index()
+        {
+            var allPatrons = _patron.GetAll();
+            var patronModels = allPatrons.Select(p => new PatronDetailModel
+            {
+
+            });
+            return View();
+        }
+    }
+}
---
+++ LibraryServices/PatronService.cs
@@ -0,0 +1,74 @@
+﻿using LibraryData;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryServices
+{
+    public class PatronService : IPatron
+    {
+        private LibraryContext _context;
+
+        public PatronService(LibraryContext context)
+        {
+            _context = context;
+        }
+        public void Add(Patron newPatron)
+        {
+            _context.Add(newPatron);
+            _context.SaveChanges();
+        }
+
+        public Patron Get(int id)
+        {
+            return GetAll()
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Patron> GetAll()
+        {
+            return _context.Patrons
+                .Include(p => p.LibraryCard)
+                .Include(p => p.HomeLibraryBranch);
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId)
+        {
+            var cardId = GetCardId(patronId);
+            return _context.CheckoutHistories
+                .Include(ch => ch.LibraryCard)
+                .Include(ch => ch.LibraryAsset)
+                .Where(ch => ch.LibraryCard.Id == cardId)
+                .OrderByDescending(ch => ch.CheckedOut);
+        }
+
+        public IEnumerable<Checkout> GetCheckouts(int patronId)
+        {
+            var cardId = GetCardId(patronId);
+            return _context.Checkouts
+                .Include(c => c.LibraryCard)
+                .Include(c => c.LibraryAsset)
+                .Where(c => c.LibraryCard.Id == patronId);
+        }
+
+        public IEnumerable<Hold> GetHolds(int patronId)
+        {
+            var cardId = GetCardId(patronId);
+            return _context.Holds
+                .Include(h => h.LibraryCard)
+                .Include(h => h.LibraryAsset)
+                .Where(h=> h.LibraryCard.Id == cardId)
+                .OrderByDescending(h=> h.HoldPlaced);
+        }
+
+        private int GetCardId(int patronId)
+        {
+            return GetAll()
+                .FirstOrDefault(p => p.Id == patronId)
+                .LibraryCard.Id;
+        }
+    }
+}
